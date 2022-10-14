9e9297bfe15475cf027befa42cf21632f3283c7e
PabloJermin <jerminamartey@gmail.com>
2022-10-14 16:20:44 -0700

    multiple lines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	print( "Size of a char: %d byte(s)\n", sizeof(char));
 
-	print(	"Size of an int: %d byte(s)\n" sizeof(int));
+	print(	"Size of an int: %d byte(s)\n", sizeof(int));
 	
-	print(	"Size of a long int: %d byte(s)\n" sizeof(long int));
+	print(	"Size of a long int: %d byte(s)\n", sizeof(long int));
 
-	print(	"Size of a long long int: %d byte(s)\n" sizeof(long long int));
+	print(	"Size of a long long int: %d byte(s)\n", sizeof(long long int));
 
-	print(	"Size of a float: %d byte(s)\n" sizeof(float));
+	print(	"Size of a float: %d byte(s)\n", sizeof(float));
 		
 	return (0)
 }
