5ad09d71236aae47d387c7f8725b7e42671b7405
PabloJermin <jerminamartey@gmail.com>
2022-10-14 08:57:51 -0700

    printing with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+	char comments[] = "Programming is like building a multilingual puzzle\n";
+	puts(comments);
+	return (0);
+
+}
