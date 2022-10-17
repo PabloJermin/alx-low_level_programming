3e74fc86114abb4afb051a8dd13b65a2af33e646
PabloJermin <jerminamartey@gmail.com>
2022-10-17 04:05:05 -0700

    put char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-This outputs an alphabet onto the screen
+ * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+	char n;
+
+	for (n <= "z")
+		putchar(n);
+	putchar("\n");
+	returrn (0);
+}
