4b99934e15bc204494adff98bb2e997bc9b3afe1
PabloJermin <jerminamartey@gmail.com>
2022-10-17 06:06:16 -0700

    excluding number

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-displays all alphabets but e and q
+ * Return: This ruturns all alphabets excluding two of them
+ */
+int main(void)
+{
+	char lcs = 'a';
+
+	while (lcs <= 'z')
+	{
+		if(lcs !='e'; && lcs != 'q')
+		{
+			putchar(lcs);
+			lcs++;
+		}
+	}
+	return (0);
+}
