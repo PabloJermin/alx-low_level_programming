4467a3747fb4b1f039a62c3ebbeff172d1d5acb0
PabloJermin <jerminamartey@gmail.com>
2022-10-18 07:59:26 -0700

    testing a function

---
+++ 0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-Testing the function
+ * Return: Success
+ */
+int main(void)
+
+{
+	        int i;
+	        i = 98;
+
+	        positive_or_negative(i);
+
+	        return (0);
+
+}
