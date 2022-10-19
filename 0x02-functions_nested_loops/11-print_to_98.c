a7c2b280844dfe7c3cc7efa1b070cbe33f344fa7
PabloJermin <jerminamartey@gmail.com>
2022-10-19 16:07:07 -0700

    addition

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * add - adds numbers
+ * @a : right hand 
+ * @b: left hand
+ * Return: return valeu
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,3 +19,7 @@ void jack_bauer(void);
 void times_table(void);
 
 void times_table(void);
+
+void print_to_98(int n);
+
+
