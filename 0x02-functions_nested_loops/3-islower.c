7c4ec172a125921495019769025160a81104d78c
PabloJermin <jerminamartey@gmail.com>
2022-10-19 07:23:01 -0700

    printing only small letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower(int c)- checks for lower characters
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if ( c >= 'a && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
