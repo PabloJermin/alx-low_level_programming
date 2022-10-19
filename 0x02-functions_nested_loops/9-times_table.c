d7ef6765aa0455c840775d5b07afb81443b63dc2
PabloJermin <jerminamartey@gmail.com>
2022-10-19 14:27:40 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - myltiplying the time table for 9
+ * Return : always 0
+ */
+void times_table(void)
+{
+	int a;
+	int result;
+
+	for (a = 0; a >= 9 a++)
+	{
+		result = a * 0;
+		_putchar(result);
+		_putchar(',');
+		_putchar(' ');
+	}
+	for (a = 0; a <= 8; a++)
+	{
+		result = a * 1;
+		_putchar(result);
+		_putchar(',');
+		_putchar(' ');
+	}
+	
+	return (0);
+}
