41e59dfc39f20f9627fd8cd28e4f0d9a7bddea4e
PabloJermin <jerminamartey@gmail.com>
2022-10-19 07:42:21 -0700

    characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_isalpha - checks for lower or upper char
+ * @c : checking for lowr characters
+ * Return : 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		retunr (0);
+	}
+}
