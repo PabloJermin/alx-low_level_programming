9b9bc6e654c8eae131ab5d0acf873ca08502f874
PabloJermin <jerminamartey@gmail.com>
2022-10-19 16:14:49 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main"
+/**
+ * add a function to add two munbers
+ * @a - an argument
+ * @b - argument on the right
+ * Return : returns the result
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
+
