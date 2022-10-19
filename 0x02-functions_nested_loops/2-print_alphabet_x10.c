5a6581e8ca0817b43c17bf58f5190968722be00e
PabloJermin <jerminamartey@gmail.com>
2022-10-19 06:07:02 -0700

    multiple alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10-printing alphabet 10x
+ * return : 0
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	char long_alpha;
+
+	long_alpha = alpha * 10;
+	_putchar(long_alpha);
+	_putchar('\n');
+
+	return (0);
+}
