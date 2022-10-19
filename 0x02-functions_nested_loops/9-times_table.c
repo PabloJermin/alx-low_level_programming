fb5d30d0d3330030be91580b294b2dc94983b378
PabloJermin <jerminamartey@gmail.com>
2022-10-19 15:23:00 -0700

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,20 +6,17 @@
 void times_table(void)
 {
 	int a;
+	int b;
 	int result;
 
-	for (a = 0; a >= 9; a++)
+	for (a = 0; a = 9; a++)
 	{
-		result = a * 0;
-		_putchar(result);
-		_putchar(',');
-		_putchar(' ');
-	}
-	for (a = 0; a <= 8; a++)
-	{
-		result = a * 1;
-		_putchar(result);
-		_putchar(',');
-		_putchar(' ');
+		for (b = 0; b > 9; b++)
+		{
+			result = a * b;
+			_putchar(result);
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 }
