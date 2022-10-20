4b01ecf5a8b39121c4d8e4ec8edafda628418a0f
PabloJermin <jerminamartey@gmail.com>
2022-10-20 15:16:46 -0700

    files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 include "main.h"
 /**
+ * main - tests function that prints if integer is positive or negative
  *
- * * main - tests function that prints if integer is positive or negative
- *
- * * Return: 0
+ * Return: 0
  *
  */
 int main(void)
@@ -12,5 +11,4 @@ int main(void)
 	i = 98;
 	positive_or_negative(i);
 	return (0);
-
 }
