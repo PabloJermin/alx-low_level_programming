fc173dafe9667d864dbe7d962a72d54ba271db38
PabloJermin <jerminamartey@gmail.com>
2022-10-20 06:20:35 -0700

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			result = a * b;
-			_putchar("%d", result);
+			printf("%d", result);
 			_putchar(',');
 			_putchar(' ');
 			_putchar(' ');
