3b2222f4a3c4e8fe0cc5710d047bcbeba3ada0ab
PabloJermin <jerminamartey@gmail.com>
2022-10-21 06:04:41 -0700

    upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio>
+/**
+ * _isupper : chesks for upper case letter
+ * @c : an argument of integer
+ * Return : returns 1 if true or 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
