9a45c5adcf10a6db0e88d5d9216b7b650f916fc5
PabloJermin <jerminamartey@gmail.com>
2022-10-21 08:55:20 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio>
+#include "main"
+/**
+ * _isdigit - looks out for a number
+ * @c: am iargument to check for a number
+ * Return: 1 if there is a number and 0 if there is no number
+ */
+int _isdigit(int c)
+{
+	if (c >= 48; && c <= 58)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
