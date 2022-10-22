6ec5901b22e7eb705356436169804cc5f2762abb
PabloJermin <jerminamartey@gmail.com>
2022-10-22 07:32:11 -0700

    addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,7 +5,7 @@
  * add - addition of two variables
  * @a: assign a value to a
  * @b: assign a value to b
- * Return : the sum of the numbers
+ * Return: the sum of the numbers always 1
  */
 int add(int a, int b)
 {
