215d7aa451caf630ff7d07151c8aa2860684038d
PabloJermin <jerminamartey@gmail.com>
2022-10-21 17:10:37 -0700

    some

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - To print some numbers
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++;)
+	{
+		if (x != 2; || x != 4;)
+		{
+			_putchar(x);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	_putchar('\n')
+}
