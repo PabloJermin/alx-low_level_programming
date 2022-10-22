5d9c529dfb66aa60620fba9ca2efa7c3e7a1d19d
PabloJermin <jerminamartey@gmail.com>
2022-10-21 17:33:17 -0700

    pirnt

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,14 @@ void more_numbers(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 10; x++;)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 15; y++;)
+		for (y = 0; y < 15; y++)
 		{
 			_putchar(y);
 		}
 		_putchar(x);
-	}
 
 	_putchar('\n');
+	}
 }
