56bbde4e0e8cbef363ca2ab54182d2babb128e19
PabloJermin <jerminamartey@gmail.com>
2022-10-23 12:35:25 -0700

    fd

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio>
+#include "main"
+/**
+ * main - printing numbers
+ *Returns: fizzbuzz
+ */
+int main (void)
+{
+	int x;
+	for (x = 1; x <= 100; x++)
+	{
+		if(x % 3 == 0 && x % 5 == 0)
+		{
+		_putchar('FizzBuzz');
+		_putchar(' ');
+		}
+		else if (x % 3 == 0)
+		{
+			_putchar('Fizz');
+			_putchar(' ');
+		}
+		else if (x % 5 == 0)
+		{
+			_putchar('Buzz');
+			_putchar(' ');
+		}
+		else
+		{
+			printf("%i", x);
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+
+	return (0);
+}
