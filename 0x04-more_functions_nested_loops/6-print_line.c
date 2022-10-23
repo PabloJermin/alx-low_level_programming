3f72908afe3e0bab7f81161680545baa15e30ea5
PabloJermin <jerminamartey@gmail.com>
2022-10-23 10:21:18 -0700

    gta

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,16 @@
  */
 void print_line(int n)
 {
-	int x;
-
-	for (x = 0; x <= n; x++)
+	if (n <= 0)
 	{
-		if (n <= 0)
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+		for (x = 0; x >= n; x++)
 		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar(_);
+			_putchar('_');
 		}
 		putchar('\n');
 	}
