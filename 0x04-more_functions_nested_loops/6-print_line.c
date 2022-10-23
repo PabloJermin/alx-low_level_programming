83d512658783a737f177eb3ea2ffa8a998be10ff
PabloJermin <jerminamartey@gmail.com>
2022-10-23 10:37:43 -0700

    we

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,18 @@
  */
 void print_line(int n)
 {
+	int x;
+
 	for (x = 0; x == n; x++)
 	{
 		if (n <= 0)
 		{
-			_putchar('\n');
+
 		}
 		else
 		{
 			_putchar('_');
 		}
-		_putchar('_');
+		_putchar('\n');
 	}
 }
