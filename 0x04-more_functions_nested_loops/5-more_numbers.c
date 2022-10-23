521b49cc805b94239914bb39bd280141559ba9ad
PabloJermin <jerminamartey@gmail.com>
2022-10-23 05:26:41 -0700

    df

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 {
 	int x, y, mod, rem;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x <= 10; x++)
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y <= 14; y++)
 		{
 			mod = y % 10;
 			rem = y / 10;
