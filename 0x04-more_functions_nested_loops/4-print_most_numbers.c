60dde34a8b82e95dde360ae0d692c621fa880392
PabloJermin <jerminamartey@gmail.com>
2022-10-23 02:51:07 -0700

    fed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_most_numbers(void)
 {
-	int x = 0;
+	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 48; x <= 58; x++)
 	{
-		if (x = 2 || x = 4;)
+		if (x = 50 || x = 52)
 		{
 			_putchar('\n');
 		}
