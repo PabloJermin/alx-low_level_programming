017b3a1b371e8b5e6c16627da75be3b10905ca2d
PabloJermin <jerminamartey@gmail.com>
2022-10-23 02:00:57 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <ctype.h>
 #include <stdio.h>
+
 /**
  * _isupper - chesks for upper case letter
  * @c: an argument of integer
  * Return : returns 1 if true or 0
- */
+ * */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
