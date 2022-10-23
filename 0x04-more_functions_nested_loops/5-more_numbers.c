9a1c3b7ba3e34e4165d5c1a01e4503bd53a752a7
PabloJermin <jerminamartey@gmail.com>
2022-10-23 05:01:29 -0700

    fds

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,23 @@
  */
 void more_numbers(void)
 {
-	int x;
-	int y;
+	int x, y, re;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 15; y++)
 		{
-			_putchar(y);
-		}
-		_putchar(x);
+			re = y % 10;
 
-	_putchar('\n');
+			if (y > 9)
+			{
+				_putchar(y + re);
+			}
+			else
+			{
+				_putchar(y);
+			}
+		}
+		_putchar('\n');
 	}
 }
