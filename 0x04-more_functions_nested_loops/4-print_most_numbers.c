79181084722e27e798a1635716db64a416e5e3f0
PabloJermin <jerminamartey@gmail.com>
2022-10-24 06:45:40 -0700

    numbes without some

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,15 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 48; x <= 57; x++)
 	{
-		if (x == 2 || x == 4)
+		if (x == 50 || x == 52)
 		{
 		}
 		else
 		{
 			_putchar(x);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
