5e7afbaf6f73b6a589df6871daa36774e6bb48d5
PabloJermin <jerminamartey@gmail.com>
2022-10-25 03:52:24 -0700

    print bar

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,12 +7,13 @@
 void print_diagonal(int n)
 {
 	int i, j;
-	
+	bar = '\\';
+
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 1; j <= n; j++)
 		{
-			_putchar("\\");
+			_putchar(bar);
 		}
 		_putchar('\n');
 	}
