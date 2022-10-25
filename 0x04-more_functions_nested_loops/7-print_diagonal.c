971722a76c751c9d926ebb2a733526184ea8d0dd
PabloJermin <jerminamartey@gmail.com>
2022-10-25 02:41:03 -0700

    printing diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_diagonal - printing diagonal lines
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	bar = \
+	for (int i = 0; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			_putchar(bar);
+		}
+		_putchar('\n');
+	}
+}
