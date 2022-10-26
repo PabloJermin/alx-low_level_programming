d46461bb7fb6462b6eb1104d87f796470dcc10f9
PabloJermin <jerminamartey@gmail.com>
2022-10-26 06:48:38 -0700

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,22 @@
+#include "main"
+/**
+ * print_times_table - printing times table
+ * @n: the parameter for the number of times
+ * Return: always 0
+ */
+void print_times_table(int n)
+{
+	int i,j;
+
+	for (i = 0; i <= n; i++)
+		for (j = 0; j = i * n; j++)
+		{
+			if (n > 15 || n < 0)
+			{
+			}
+			else
+			{
+				_putchar(j);
+			}
+		}
+}
