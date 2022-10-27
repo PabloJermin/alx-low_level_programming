b1c404b5f1c761c33490b6285ecc8b81f5f3eef2
PabloJermin <jerminamartey@gmail.com>
2022-10-27 08:56:19 -0700

    reverse mode

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - for reversing strings
+ * @s: a parameter for the function
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int b = 0;
+	int k = s[b];
+	int a;
+	i = 0;
+
+	while (k != '\0')
+	{
+		b++;
+		i++;
+	}
+	for (a = i; a >= 0; a--)
+	{
+		_putchar(s[i]);
+	}
+}
