954cb482e4e4ea1a7a48b285e5e9cd783d1f02fc
PabloJermin <jerminamartey@gmail.com>
2022-10-27 06:18:00 -0700

    printing the text

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string
+ * @: str: a string parameter for the function
+ * Return: Returns a number
+ */
+void _puts(char *str)
+{
+	char *a;
+	
+	a = &s;
+	printf(a);
+	printf("\n");
+}
