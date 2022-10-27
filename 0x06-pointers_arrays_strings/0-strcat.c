a93678749f991e8280167c33bfdb8b099a98598d
PabloJermin <jerminamartey@gmail.com>
2022-10-27 14:27:32 -0700

    joining texts

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcat - strings together
+ * @dest: an argumetn to the function
+ * @src: an aurgument for the function
+ * Retun: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	char  dest = a;
+	char src = b;
+
+	_putchar(a + " " + b);
+}
