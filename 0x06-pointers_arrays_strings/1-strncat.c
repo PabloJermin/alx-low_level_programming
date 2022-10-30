23b76ef3a29929696a23fc0f0c0a1cef7faf5b42
PabloJermin <jerminamartey@gmail.com>
2022-10-30 15:01:56 -0700

    two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,7 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
@@ -18,7 +18,6 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		dest[i + j] = src[j];
 	}
-	dest[i +j] = '\0';
-	
+	dest[i + j] = '\0';
 	return (dest);
 }
