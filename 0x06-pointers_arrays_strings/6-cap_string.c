ad2e78dc72ad02e3d97b12a252701f17244f3ac4
PabloJermin <jerminamartey@gmail.com>
2022-10-30 16:04:20 -0700

    changing all to upper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizing words of string
+ * @s: a parameter for the function
+ * Return: Always a number
+ */
+char *cap_string(char *)
+{
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + 1) -+ 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
