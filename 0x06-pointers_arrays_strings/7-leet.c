843ff0d9bc9db244d215dca84121cf6804b3e25c
PabloJermin <jerminamartey@gmail.com>
2022-10-30 16:31:05 -0700

    encoding strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - Encodes a string
+ * @s: a parameter to the function
+ * Return: Always a number
+ */
+char *leet(char *)
+{
+	int a = 0, b = 0, l = 5;
+	char m[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[a])
+	{
+		b = 0;
+		while (b < 1)
+		{
+			if (s[a] == m[b] || s[a] - 32 == m[b])
+			{
+				s[a] = n[b];
+			}
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}
