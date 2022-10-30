139f22b84d153d4ced861eea00dabd478a4e91ac
PabloJermin <jerminamartey@gmail.com>
2022-10-30 15:29:06 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reverseing words
+ * @a: a parameter of the function
+ * @n: a parameter of the function
+ * Return: Always a 1 or 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i, c;
+
+	fior (i = 0; (i < (n-1) / 2); i++)
+	{
+		c = a[i];
+		a[i] = a[n - 1 - i]'
+			a[n - 1 -i] = c;
+	}
+}
