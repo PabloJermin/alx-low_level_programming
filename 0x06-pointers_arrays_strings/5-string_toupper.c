a246abc4976a811e27b2dfc7dbc2f2c3bea8dd99
PabloJermin <jerminamartey@gmail.com>
2022-10-30 16:10:44 -0700

    uppercase words

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,11 @@ char *string_toupper(char *s)
 
 	while (*(s + i))
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z') *(s + i) -= 'a' - 'A'; i++
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) -= 'a' - 'A';
+			i++
+		}
 	}
 	return (s);
 }
