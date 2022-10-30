bbf744c72bcdecd46e64e5eb8aa9c6b8c5a5f5e8
PabloJermin <jerminamartey@gmail.com>
2022-10-30 15:46:29 -0700

    changing cases

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changing lower case letters to upper cases
+ * @s: a parameter
+ * Return: Alwasy 0 or 1
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(a + i) <= 'z') *(s + i) -= 'a' - 'A'; i++
+	}
+	return (s);
+}
