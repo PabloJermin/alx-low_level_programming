de48c68481103a3f5db64361b66a7a4eff5e9910
PabloJermin <jerminamartey@gmail.com>
2022-11-01 13:08:07 -0700

    setting a memory to another memory

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,7 +11,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
 
-	for (i = 0; n1[i]; i++;)
+	for (i = 0; n1[i]; i++)
 		;
 	for (j = 0; n2[j]; j++) 
 		;
@@ -21,7 +21,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
-		if(i >= o)
+		if(i >= 0)
 			n += n1[i] - '0';
 		if (j >= 0)
 			n += n2[j] - '0';
@@ -36,12 +36,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (i >= 0 || j >= 0 || m)
 		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
-	{k -= 1, l = 0; l < k; k--, l++)
-		{
-			m = r[K];
-			r[k] = r[l];
-			r[l] =m;
-		}
-		return (r);
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
 	}
+	return (r);
 }
---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills a memory with a constant byte
+ * @s: a parameter to the function
+ * @b: a parameter to the function
+ * @n: a parameter to the function
+ * Return: Always 1 or 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *rep;
+	
+	for (i = 0; i <= n; i++)
+	{
+		rep  += *s[i];
+	}
+	return (*rep = b);
+}
