ffcdd6fbe30c9bfe1a16743293469f2ddf2c538f
PabloJermin <jerminamartey@gmail.com>
2022-11-02 07:20:50 -0700

    printing characters

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - a recursionn fo characters
+ * @s: a variable to the function
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
