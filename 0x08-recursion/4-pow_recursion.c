4908494cf1a9583bfb43a190e05a9457c0f9113f
PabloJermin <jerminamartey@gmail.com>
2022-11-04 03:51:30 -0700

    exponents

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - exponenntial power
+ * @x: the parameter of the function
+ * @y: the parameter of the function
+ * Return: always a number
+ */
+int _pow_recursion(int x, int y)
+{
+	int result;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else 
+		result = _pow_recursion(x) ** y;
+
+	return (result);
