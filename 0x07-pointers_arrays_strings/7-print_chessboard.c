37c4f8c40d128e3a919af8dc75655745f9ac69ba
PabloJermin <jerminamartey@gmail.com>
2022-11-06 12:46:54 -0800

    printing chaess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chess board
+ * @a: a parameter to the function
+ * Return:Returns chaacters to the function
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i,j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar(10);
+	}
+}
