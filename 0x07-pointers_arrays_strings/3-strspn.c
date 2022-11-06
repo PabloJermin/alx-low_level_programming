23d1ebc45dd8c197be4092d60cb296a4285fe5f7
PabloJermin <jerminamartey@gmail.com>
2022-11-06 11:15:38 -0800

    counting chars

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,20 +8,16 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, k;
+	k = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		k = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				k = k + 1;
 			}
-		}
-		if ( k == 0)
-		{
-			break;
 		}
 	}
 	return (k);
