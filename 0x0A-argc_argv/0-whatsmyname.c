647541c598e7156a3d31e83ce19f134d75d1b973
PabloJermin <jerminamartey@gmail.com>
2022-11-07 05:54:50 -0800

    printing the name of file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * main - a function to 
+ * main - a function to
+ * @arg: parameter for the name of file
+ * @argv: parameter for the position of file name 
  * Return : Always a number
  */
 int main (int argc_attribute_((unused)), char *argv[])
