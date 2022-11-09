a5b45d94d301a8e88090f3334ad54f345fd2359c
PabloJermin <jerminamartey@gmail.com>
2022-11-09 06:34:35 -0800

    multiplying two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that multiplies two arguments
+ * @argc: an argument to the function
+ * @argv: second argument to the function
+ * Return: returns always 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int numb1 = atoi(argv[0]);
+	int numb2 = atoi(argv[1]);
+	int result;
+
+	result = numb1 * number2;
+	printf("%d\n", result);
+
+	return (0);
+}
