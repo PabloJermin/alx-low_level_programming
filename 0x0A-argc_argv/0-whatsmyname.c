955bc3dc8591e6892b791d4f885ca74f37e72c2b
PabloJermin <jerminamartey@gmail.com>
2022-11-09 06:09:08 -0800

    printing a name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - a function to print name of program
  * @argc: parameter for the name of file
  * @argv: parameter for the position of file name
- * Return: Always a number
+ * Return: Always a number 0 or 1
  */
 int main (int argc, char *argv[])
 {
