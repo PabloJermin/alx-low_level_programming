c8adfcccb9ca4cbca691dba2d8e8e6fbdd695614
PabloJermin <jerminamartey@gmail.com>
2022-11-09 05:33:53 -0800

    printing name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argv: parameter for the position of file name
  * Return : Always a numbe
  */
-int main (int argc_attribute_((unused)), char *argv[])
+int main (int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
