7323870857d986650ab0e45c1f9727cfa9d46926
PabloJermin <jerminamartey@gmail.com>
2022-11-11 07:27:46 -0800

    more fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - program that finds and prints the sum of the even-valued
+ * Return: the dum of evaluated number
+ */
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sums;
+	float to_sum;
+
+	while (1)
+	{
+		fibsum = fib1 + fib2;
+		if (sums > 4000000)
+			break;
+		if ((sums % 2) == 0)
+			to_sum += fibsum;
+
+		fib1 = fib2;
+		fib2 = sums;
+	}
+	printf("%.0f\n", to_sum);
+
+	return (0);
+}
+
