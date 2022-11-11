a0aebd1564d52ee4d750712b7da07c9f2f1e6110
PabloJermin <jerminamartey@gmail.com>
2022-11-11 04:33:33 -0800

    joining two words

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,6 +27,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (d == NULL)
 		return (NULL);
+	for (k = 0; s1[k] != '\0'; k++)
+		d[k] = s1[k];
+
 	for (k = 0; k < n; k++)
 		d[k + i] = s2[k];
 	d[k + i] = '\0';
