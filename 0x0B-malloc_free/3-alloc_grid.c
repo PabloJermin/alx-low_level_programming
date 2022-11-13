e4bec820a6bb71db13dd8202304f4d08934fa0e0
PabloJermin <jerminamartey@gmail.com>
2022-11-13 12:32:42 -0800

    allocation of memory to an array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid -  a pointer to a two dimentional array.
+ * @width: n argument to the function
+ * @height: another argument to the pointer
+ * Return: returns a two dimentional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int *p;
+
+	for (i = 0; i <= height; i++)
+		for (j = 0; j <= width; j++)
+		{
+			p = malloc(sizeof(int) * width);
+		}
+	return (p);
+}
+
