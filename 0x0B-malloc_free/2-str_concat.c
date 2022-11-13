4b1963a3804e34b223eb148386cd75786450ac4a
PabloJermin <jerminamartey@gmail.com>
2022-11-13 05:31:49 -0800

    concatenating two characters

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concatenating two strings
+ * @s1: an argument of character to the function
+ * @s2: a second argument of character to the function
+ * Return: always a charater or null
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p, s3;
+	int len = 1;
+
+	if (s1 == NULL || s2 = NULL)
+		return (NULL);
+	for (i = 0; s1 != '\0'; i++)
+	{
+		len =+ i;
+		s3 =+ s1[i];
+	}
+	for (i = 0; s2 != '\0'; i++)
+	{
+		len =+ i;
+		s3 =+ s2[i];
+	}
+	p = malloc(sizeof(s1)*len);
+	for (i = 0; i <= len; i++)
+		p = s3;
+	return (p);
+}
