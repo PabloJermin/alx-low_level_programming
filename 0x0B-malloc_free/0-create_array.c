b418ae0a5f93caed0d1a30c4d33afd9394555308
PabloJermin <jerminamartey@gmail.com>
2022-11-13 01:00:02 -0800

    allocation of a memory with initialization

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_array - creating an array of char
+ * @size: the size of the array
+ * @c: the character to be mapped to
+ * Return: always a number
+ */
+*create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size > 0)
+	{
+		p = malloc(size * sizeof(char));
+		if (p == NULL)
+			return (NULL);
+	}
+	else
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
