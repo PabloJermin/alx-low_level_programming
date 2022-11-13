405b8dc4b7013c67385da1368a4795a4f8f657f4
PabloJermin <jerminamartey@gmail.com>
2022-11-13 12:44:15 -0800

    malloc of an array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,11 +12,19 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int *p;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i <= height; i++)
+	{
 		for (j = 0; j <= width; j++)
 		{
 			p = malloc(sizeof(int) * width);
 			p[j] = 0;
 		}
+		return (*p);
+	}
 }
 
