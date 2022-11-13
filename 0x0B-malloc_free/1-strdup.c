983c4f9001e5f28cd54e5af7ba29791785e4d513
PabloJermin <jerminamartey@gmail.com>
2022-11-13 02:28:51 -0800

    making a copy of a character and returning as a pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stio.h>
+#include <stdlib.h>
+/**
+ * _strdup - a function that returns a pointer to a string in a copy
+ * @str: an argument to the function
+ * Return: returns a pointer to the arfunction
+ */
+char *_strdup(char *str)
+{
+	char *str1;
+	char *p;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[len]; len++)
+		;
+	str1 = malloc(len + 1);
+	p = str1;
+	if (str1 == NULL)
+		return (NULL);
+	while (*str)
+		*p++ = *str++;
+	*p = '\0';
+	return (str1);
+}
