0b4db3da7c77f88772d8f674f0aca0058a091c66
PabloJermin <jerminamartey@gmail.com>
2022-11-16 02:34:08 -0800

    function-ke macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-/**main - functionlike macros
- * Return: always a success
- */
-#ifndef _function-Like_macros_h_
+#ifndef _function-like_macros_h_
 #define _function_like_macros_h_
 #define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 #endif/*_function_like_macros_h_*/
