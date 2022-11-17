c6a3ea199a19f62a6c0c08ad6090902a257cc217
PabloJermin <jerminamartey@gmail.com>
2022-11-17 14:47:19 -0800

    t
    git push
    git add .
    git commit -m t

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H
 /**
  * print_dog - a function struct to print  the name of a deg
  * @d: an argument to get the name of the dog
@@ -8,15 +10,12 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		return;
-	if (d->name == NULL)
-		printf("Name: %s\n", d->name);
-	else 
-		printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-		printf("owner: (nil)\n");
+	{
+	}
 	else
-		printf("owner: %s\n", d->owner);
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %d\n", (d->age) ? d->age : 0);
+		printf("Ownwe: %s\n", (d->owner) ? d->owner : "(nil)");
 }
-
+#endif
