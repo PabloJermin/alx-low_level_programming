3a7fff8177d54e198687cf9c82282ebfa71af385
PabloJermin <jerminamartey@gmail.com>
2022-11-17 13:18:05 -0800

    function-like macros

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _function-like_macros_h_
-#define _function_like_macros_h_
+#ifndef _FUNCTION_LIKE_MACROS_H_
+#define _FUNCTION_LIKE_MACROS_H_
 #define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 #endif/* _function_like_macros_h_ */
