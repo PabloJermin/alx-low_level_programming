a63ecdaddb594cb62ddde52dd223a75747e8a0de
PabloJermin <jerminamartey@gmail.com>
2022-11-18 06:04:30 -0800

    summing all arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - a variable function that sums all the arguments
+ * @n: the number of arguments
+ * Return: returns a number
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+
+	va_list arg;
+	va_start (arg, n);
+	for (i = 0; i <= n; i++)
+	{
+		int j = va_arg (arg, int);
+		sum = sum + j;
+	}
+	va_end (arg);
+}
