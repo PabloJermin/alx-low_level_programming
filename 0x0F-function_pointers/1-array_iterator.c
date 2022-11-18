fd8efeb65bf16e263660b2a35fd6038dc7f7a346
PabloJermin <jerminamartey@gmail.com>
2022-11-18 15:49:59 -0800

    array function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,11 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int a;
+
 	if (array = NULL || action == NULL)
 		return;
-	while (size-- > 0)
+	for (a = 0; a < size; a++)
 	{
-		action(*array);
-		array++;
-	}
+		action(array[a]);
 }
