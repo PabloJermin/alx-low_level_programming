ac216c01157f7d431149d915b2c46ebc07143365
PabloJermin <jerminamartey@gmail.com>
2022-11-23 07:21:08 -0800

    initila\ing a function with with an interger

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,15 +9,6 @@
  * @owner: the owner of the dog
  * Return: alwasy a success
  */
-#ifdef _STRUCT_DOG_H
-#define _STRUCT_DOG_H_
-#define struct dog
-{
-	char name;
-	float age;
-	char owner;
-}
-#endif
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
