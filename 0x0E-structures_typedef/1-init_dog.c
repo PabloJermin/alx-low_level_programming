3a601b5cde159b885ac0d8bcd7812758b0f068c4
PabloJermin <jerminamartey@gmail.com>
2022-11-23 07:13:40 -0800

    initiali\ing the function with struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -20,10 +20,11 @@
 #endif
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		return;
 	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
