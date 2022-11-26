5f2d0b43d3bd9959f93c1df4189f18e3bcfe8e40
PabloJermin <jerminamartey@gmail.com>
2022-11-26 05:32:41 -0800

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,44 @@
 #include "variadic_functions.h"
 /**
- * print_all - prints anything.
- * @format: a list of types of arguments passed to the function.
- * Return: no return.
+ * print_all - prints anything
+ * @format: format of data type to be printed
  */
 void print_all(const char * const format, ...)
 {
-	va_list valist;
-	unsigned int i = 0, j, c = 0;
-	char *str;
-	const char t_arg[] = "cifs";
+	unsigned int i = 0;
+	char *str, *sep = "";
+	va_list arguments;
 
-	va_start(valist, format);
-
-	while (format && format[i])
+	va_start(arguments, format);
+	if (format)
 	{
-		j = 0;
-		while (t_arg[j])
+		while (format[i])
 		{
-			if (format[i] == t_arg[j] && c)
+			switch (format[i])
 			{
-				printf(", ");
-				break;
-			} j++;
-		}
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(valist, int)), c = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(valist, int)), c = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(valist, double)), c = 1;
-				break;
-			case 's':
-				str = va_arg(valist, char *), c = 1;
-				if (!str)
-				{
-					printf("(nil)");
+				case 'c':
+					printf("%s%c", sep, va_arg(arguments, int));
 					break;
-				}
-				printf("%s", str);
-				break;
-		} i++;
+				case 'i':
+					printf("%s%i", sep, va_arg(arguments, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(arguments, double));
+					break;
+				case 's':
+					str = va_arg(arguments, char *);
+					if(!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
 	}
-	printf("\n"), va_end(valist);
+	printf("\n");
+	va_end(arguments);
 }
