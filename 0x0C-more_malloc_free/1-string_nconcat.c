c71d422abc263591a9fd374e96d920bf95fdfe58
PabloJermin <jerminamartey@gmail.com>
2022-11-26 13:24:45 -0800

    a function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,38 +1,40 @@
-#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - to concatenatetwo strings
- * @s2: an argument to the function
- * @s1: a second arugument to the functio
- * @n: the number of arguments 
- * Return: Always 0;
+ * string_nconcat - Main Entry
+ * @s1: input
+ * @s2: input
+ * @n: input
+ * Return: 0
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k;
-	char *d = NULL;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = "";
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
 	if (s2 == NULL)
-		s2 ="";
-	for (i = 0; s1[1] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	if (j <= n)
-		n = j;
-	d = malloc((i + n + 1) * size of (char));
-
-	if (d == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
 		return (NULL);
-	for (k = 0; s1[k] != '\0'; k++)
-		d[k] = s1[k];
-
-	for (k = 0; k < n; k++)
-		d[k + i] = s2[k];
-	d[k + i] = '\0';
-
-	return (d);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
