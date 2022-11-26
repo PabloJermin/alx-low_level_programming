d8a7f9e23ab9f0edf2b030e40a9b2e4322d35cbf
PabloJermin <jerminamartey@gmail.com>
2022-11-26 14:23:10 -0800

    a function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,31 +1,37 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdio.>
 #include <stdlib.h>
 /**
- * str_concat - concatenating two strings
- * @s1: an argument of character to the function
- * @s2: a second argument of character to the function
- * Return: always a charater or null
+ * str_concat - Main Entry
+ * @s1: input
+ * @s2: input
+ * Return: 0
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	int len = 0, i = 0;
-
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 = " ";
-	for (len = 0; s1[len] || s2[len]; len++)
-		;
-	s3 = malloc(sizeof(s1) * len);
-	if (s3 == NULL)
+	unsigned int size1 = 0, size2 = 0;
+	char *ptr, *ret;
+	ptr = s1;
+	
+	if (s1)
+		while (*ptr++)
+			size1++;
+	else
+		s1 = "";
+	ptr = s2;
+	if (s2)
+		while (*ptr++)
+			size2++;
+	else
+		s2 = "";
+	ret = malloc(size1 + size2 + 1);
+	if (!ret)
 		return (NULL);
-	for (len = 0; s2[len]; len++)
-	{
-		s3[i++] = s1[len];
-	}
-	for (len = 0; s2[len]; len++)
-		s3[i++] = s2[len];
-	return (s3);
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = 0;
+	return (ret);
 }
