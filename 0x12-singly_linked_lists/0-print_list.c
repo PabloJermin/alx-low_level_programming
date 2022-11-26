83542db46f4c50563eb3b191519b9c611ecbe898
PabloJermin <jerminamartey@gmail.com>
2022-11-25 22:54:41 -0800

    printing all items from linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,30 +6,19 @@
  * @list_t : an argument to the function
  * @h : an argument pointer to the function
  * Return : always true
- *
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- * Description: singly linked list node structure
  */
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-
 size_t print_list(const list_t *h)
 {
-	list_t *temp = h;
-
-	while (temp != NULL)
+	size_t tem;
+	tem = 0;
+	while (h != NULL)
 	{
-		printf("%d", temp -> str);
-		printf("%d", temp -> len);
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		tem++;
 	}
-	else
-		printf("nil");
-	return (temp);
+	return (tem);
 }
