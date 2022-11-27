b4999fa7cc284ff784c846c368459d6293530a5f
PabloJermin <jerminamartey@gmail.com>
2022-11-27 08:00:06 -0800

    prime factors

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,33 +1,28 @@
-#include "main"
+#include "main.h"
 /**
- * print_triangle
- * @size: variable for size of triangle
- * Return: always 0
+ * print_triangle -  a function that prints a triangle, followed by a new line.
+ * @size: An input integer
+ * Return: Always 0
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i = 0, j, n = size - 1;
 
-	if (size <= 0)
+	if (size > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for ( i = 0; i <= size; i++)
+		for (; i < size; i++)
 		{
-			for (j = 0; j <= sixe j++)
+			for (j = 0; j < size; j++)
 			{
-				if (j == size)
-				{
-					_putchar('#');
-				}
-				else
-				{
+				if (j < n)
 					_putchar(' ');
-				}
+				else
+					_putchar('#');
 			}
+			n--;
 			_putchar('\n');
 		}
 	}
+	else
+		_putchar('\n');
 }
