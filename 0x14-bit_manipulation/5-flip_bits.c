06356792629861b627ae934b205b65e2b781f337
PabloJermin <jerminamartey@gmail.com>
2022-12-02 15:29:34 -0800

    #include main.h
    /**
    
     * flip_bits - returns the number of bits you would
    
     * need to flip to get from one number to another
    
     * @n: number one.
    
     * @m: number two.
    
     *
    
     * Return: number of bits.
    
     */
    
    unsigned int flip_bits(unsigned long int n, unsigned long int m)
    
    {
    
    unsigned int nbits;
    
    for (nbits = 0; n || m; n >>= 1, m >>= 1)
    
    {
    
    if ((n & 1) != (m & 1))
    
    nbits++;
    
    }
    
    return (nbits);
    
    }
    git add .
    git cpmmit -m

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * flip_bits - returns the number of bits you would
  * need to flip to get from one number to another
@@ -8,11 +9,11 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int nbits
-		for (nbits = 0; n || m; n >>= 1, m >>= 1)
-		{
-			if ((n & 1) != (m & 1))
-				nbits++;
-		}
+	unsigned int nbits;
+
+	for (nbits = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			nbits++;}
 	return (nbits);
 }
